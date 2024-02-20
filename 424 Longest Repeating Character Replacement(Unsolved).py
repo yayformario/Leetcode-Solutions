b07a2f4f3687a702bbef02d38afae25b4e14c932
Mario Huerta <yayformario@gmail.com>
2024-02-19 22:22:18 -0500

    Solution for 424 and notes on hashing

--- 424 Longest Repeating Character Replacement(Unsolved).py
+++ 424 Longest Repeating Character Replacement(Unsolved).py
@@ -6,28 +6,57 @@ class Solution(object):
         #Two Pointer approach
         leftPointer = 0
         replaceCount = 0
+
         #Going to need a hashmap
+        charCounters = {}
+
+        #We can initialie it with the first letter and a counter of zero
+        charCounters[s[0]] = 0
         
+        #Keep track of the most re-ocurring character
+        maxOccurence = 0
         #Loop entire array:
         for rightPointer in range(len(s)):
-            #Check if we find a new character
-            if (s[leftPointer] != s[rightPointer]):
-                replaceCount += 1
-                #Slide window to ensure we have enough
-                if (replaceCount > k):
-                    leftPointer += 1
-                    replaceCount -= 1
-            maxLen = max(maxLen, (rightPointer - leftPointer + 1))
+            #Check if letter in hashmap
+            if (s[rightPointer] in charCounters):
+                #Incrementer letter counter
+                charCounters[s[rightPointer]] = charCounters[s[rightPointer]] + 1
+                maxOccurence = max(maxOccurence, charCounters[s[rightPointer]])
+                
+            #Not in hashmap
+            else:
+                #Add the letter to our hashmap
+                charCounters[s[rightPointer]] = 1
+            
+            #[maxOccurenece + k] the largest window we can have
+            #We update maxLen as long as <= than that
+            if ((rightPointer - leftPointer + 1) <= (maxOccurence + k)):
+                    maxLen = max(maxLen, rightPointer - leftPointer + 1)
+            
+            
+            #Shrink window if it's too long for replacements
+            if ((rightPointer - leftPointer + 1) > (maxOccurence + k)):
+                #If L is the max occuring char, decrement maxOccurence
+                if (charCounters[s[leftPointer]] == maxOccurence):
+                    maxOccurence -= 1
+                #Decrement hash value
+                charCounters[s[leftPointer]] = charCounters[s[leftPointer]] - 1
+
+                #Move left pointer
+                leftPointer += 1
+
+
+
         return maxLen
     
 
 testing = Solution()
 
 examples = [
-    #Given examples
-    "ABBCDEFGHHHHJJJKKKKLMOP", 3,
-    "ABAB", 2, #expecting 4
+    "BCDAA", 1, #expecting 3
     "AABABBA", 1, #expecting 4
+    "ABAB", 2, #expecting 4
+    
 
     #k is zero test case
     "ABCD", 0, #expecting 1
@@ -38,7 +67,10 @@ examples = [
     "ABCDEFG", 7,  #expecting 7
 
     #Long middle answer"
-    "ABBCDEFGHHHHJJJKKKKLMOP", 3 #expecting 4
+    "ABBCDEFGHHHHJJJKKKKLMOP", 3, #expecting 7
+
+    #half and half
+    "AAAABBBB", 2 #Expecting 6
 
 
     
