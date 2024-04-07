cfa52d01b398922470db252303954be4ba7fee2d
Pixiestickz <23437228+Pixiestickz@users.noreply.github.com>
2024-04-07 17:25:51 -0400

    Renamed file

--- 560 Subarray Sum Equals K(My way).py
+++ 560 Subarray Sum Equals K(My way).py
@@ -12,6 +12,12 @@ class Solution:
         #{prefixSum : counter}
         prefixSumHash = {}
 
+        #Not immediately obvious but we will need a way to count the original "zero" for 1 elmeent edge cases
+        #Edge case: nums = [3], k = 3
+        #Desired value: 3 - 3 = 0 
+        #We can always subtract nothing from anything
+        #So we need to setup a default key-value pair: { 0 : 1 } 
+        prefixSumHash[0] = 1
 
         #Loop every value
         #O(n)
@@ -21,17 +27,26 @@ class Solution:
             prefixSum[i] = currentSum
 
             #Save prefixSum and how many times it's appeared 
-            if prefixSumHash[i]:
+            if prefixSum[i] in prefixSumHash:
                 prefixSumHash[prefixSum[i]] = prefixSumHash[prefixSum[i]] + 1
-            else: 
+            
+            else:
                 prefixSumHash[prefixSum[i]] = 1
+
+            #Calculate the value we're looking for
+            desiredValue = currentSum - k
+
+            #Check to see if this desiredValue is in our Hash
+            if desiredValue in prefixSumHash:
+                #By how many times we've encountered the desired value
+                #We can also safely pop the key
+                returnValue += prefixSumHash.pop(desiredValue)
+
         return returnValue
     
 testing = Solution()
 
 examples = [
-
-    [1, -1, -1, -1, 1], -1,         #Expecting 5  
     #Given examples
     [1 , 1, 1] , 2,     #Expecting 2
     [1, 2, 3], 3,       #Expecting 2
@@ -45,9 +60,10 @@ examples = [
     [1, 1], 1,      #Expecting 2
 
     #Personal edge cases
-     
-    [-1, -1, -1, -1, -1, -1], -2,   #Expecting 4
-    [1, 1, 1, 1, 1, 1], 3,          #Expecting 3
+    [1, -1, -1, -1, 1], -1,         #Expecting 6
+    [-1, -1, -1, -1, -1, -1], -2,   #Expecting 5
+    [1, 1, 1, 1, 1, 1], 3,          #Expecting 4
+
 ]
 
 for i in range(0, len(examples), 2):
