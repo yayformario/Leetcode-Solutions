488ed2ad54d3db411bf37b26a2901d9df880dda2
Pixiestickz <23437228+Pixiestickz@users.noreply.github.com>
2025-01-15 20:07:39 -0500

    still working on solution for #15

---
+++ 15 3Sum.py
@@ -0,0 +1,109 @@
+from typing import List
+
+    #Brute force: O(n * n * n)
+    #Merge: O(n log n) 
+    #Once merged, can single pass for multiple answers
+    #Is O(3n) possible? 
+    #- Without sorting, can accidentally 'skip' potential solutions
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        length = len(nums)
+
+        #prep pointers
+        base = 0
+
+        #Sort to check for early exits
+        nums.sort()
+        if nums[0] > 0 or nums[length-1] < 0:
+            return ans
+        
+        duplicates = set()
+        #Loop base; ensure other points aren't OOB
+        while base < length-2:
+            # CAN'T SKIP DUPLICATES RIGHT OFF THE BAT
+            #Potential answer reusing a variable: [-1,-1,2]
+            #Only skip if we've already seen it before
+            if nums[base] in duplicates:
+                temp = nums[base]
+                while temp == nums[base] and base < length-2:
+                    base += 1
+            
+            #Gauranteed a non duplicate value
+            #The rest is 2SUM
+            base -=1 
+            left = base + 1
+            right = length-1
+            
+            #Searching for target to ensure we're only looking for pairs
+            # 0 = base + left + right -> -base = left + right -> target = -base
+            target = -nums[base]
+            duplicates2 = set()
+            while left < right:
+                sum = nums[left] + nums[right]
+                #Skip any duplicates right off the bat
+                #offset by 1 to cover the edge case of last 3 variables
+                if sum > target:
+                    temp2 = nums[right]
+                    while temp2 == nums[right] and left<right:
+                        right -=1
+                elif sum < target:
+                    temp2 = nums[left]
+                    while temp2 == nums[left] and left < right:
+                        left +=1
+                else:
+                    #Check if we've seen solution before adding it
+                    duplicates2.add(nums[left])
+                    ans.append([nums[base], nums[left], nums[right]])
+                    left +=1
+
+            duplicates.add(nums[base])
+            base+=1
+        return ans
+
+
+
+    
+
+testing = Solution()
+
+examples = [
+
+    
+
+    #all zeros
+    [0,0,0,0,0,0,0,0,0,0,0,0], #[0,0,0]
+
+    #Solution is first 3 or last 3
+    [-2,0,2,15,15,15,15,15],#[-2,0,2]
+    [-15,-15,-15,-15,-2,0,2],#[-2,0,2]
+    [-100,-50,0,50,100],#[-100,0,100] [-50,0,50]
+
+    #No solution
+    [-3,-2,-1,12,13,14,15], #[]
+    #Solution at edges
+    [100,0,-100,-2,15,15,15,15],#[-100,0,100]
+
+
+    #Given examples
+    [-1,0,1,2,-1,-4], #[-1,-1,2], [-1,0,1]
+    [0,1,1], #[]
+    [0,0,0], #[]
+
+    
+
+    
+
+    #All positives/negatives
+    [-12,-9,-4,-100,-55], #[]
+    [12,5,67,43,50,100,101], #[]
+
+]
+
+    
+
+
+for ex in examples:
+    print(str(testing.threeSum(ex)))
+    
