4e9113171d34aadde9198a891fa400c243f7b8ad
Pixiestickz <23437228+Pixiestickz@users.noreply.github.com>
2025-02-16 22:21:26 -0500

    added file for #21

---
+++ 21 Merge Two Sorted Lists.py
@@ -0,0 +1,54 @@
+from typing import List, Optional
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+    
+
+class Solution:
+    def __init__(self, list1: List[int], list2: List[int]) -> None:
+        #Create two separate linked list
+        self.list1 = list1
+        self.list2 = list2
+
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        pass
+
+    def generateLinkedList (self, list=[]) -> ListNode:
+        pass
+
+
+examples = [
+    #given examples
+    [ [1,2,4], [1,3,4] ],
+    [ [], [] ],
+    [ [], [0] ]
+]
+
+#loop each solution
+for ex in examples:
+    len1 = len(ex[0])
+    len2 = len[ex[1]]
+
+    searching = None
+
+    testing = Solution(ex[0], ex[1])
+
+    #Create two separate linked list
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+    
